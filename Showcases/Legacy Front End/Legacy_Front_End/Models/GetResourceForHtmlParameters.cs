7947263b7ba25abf229f45fb0cd0adcecfa45c69
Muhammad Umar <muhammad.umar@groupdocs.com>
2016-04-21 21:06:31 +0500

    Upload Front Ends for 3.2.0

---
+++ Showcases/Legacy Front End/Legacy_Front_End/Models/GetResourceForHtmlParameters.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MvcSample.Models
+{
+    public class GetResourceForHtmlParameters
+    {
+        public string DocumentPath { get; set; }
+        public string ResourceName { get; set; }
+        public int PageNumber { get; set; }
+    }
+}
