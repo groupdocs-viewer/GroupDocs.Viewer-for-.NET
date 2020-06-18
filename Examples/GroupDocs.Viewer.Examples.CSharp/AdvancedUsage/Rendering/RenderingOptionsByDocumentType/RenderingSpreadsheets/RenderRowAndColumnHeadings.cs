133bfb3c4aff81595f1a13d6ca862099d14b6acb
Vladimir Litvinchik <vladimir.litvinchik@aspose.com>
2020-06-18 11:00:42 +0300

    Updated examples that show how to render spreadsheet column headings and row numbers.

--- Examples/GroupDocs.Viewer.Examples.CSharp/AdvancedUsage/Rendering/RenderingOptionsByDocumentType/RenderingSpreadsheets/RenderRowAndColumnHeadings.cs
+++ Examples/GroupDocs.Viewer.Examples.CSharp/AdvancedUsage/Rendering/RenderingOptionsByDocumentType/RenderingSpreadsheets/RenderRowAndColumnHeadings.cs
@@ -1,8 +1,6 @@
 ﻿using GroupDocs.Viewer.Options;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 
 namespace GroupDocs.Viewer.Examples.CSharp.AdvancedUsage.Rendering.RenderingOptionsByDocumentType.RenderingSpreadsheets
 {
@@ -16,13 +14,46 @@ namespace GroupDocs.Viewer.Examples.CSharp.AdvancedUsage.Rendering.RenderingOpti
             string outputDirectory = Utils.GetOutputDirectoryPath();
             string pageFilePathFormat = Path.Combine(outputDirectory, "page_{0}.html");
 
+            // TO HTML
             using (Viewer viewer = new Viewer(TestFiles.SAMPLE_XLSX))
             {
                 HtmlViewOptions options = HtmlViewOptions.ForEmbeddedResources(pageFilePathFormat);
-                options.SpreadsheetOptions = SpreadsheetOptions.ForOnePagePerSheet();
                 options.SpreadsheetOptions.RenderHeadings = true;
 
-                viewer.View(options);
+                viewer.View(options,1, 2, 3);
+            }
+
+            // TO JPG
+            pageFilePathFormat = Path.Combine(outputDirectory, "page_{0}.jpg");
+
+            using (Viewer viewer = new Viewer(TestFiles.SAMPLE_XLSX))
+            {
+                JpgViewOptions options = new JpgViewOptions(pageFilePathFormat);
+                options.SpreadsheetOptions.RenderHeadings = true;
+
+                viewer.View(options, 1, 2, 3);
+            }
+
+            // TO PNG
+            pageFilePathFormat = Path.Combine(outputDirectory, "page_{0}.png");
+
+            using (Viewer viewer = new Viewer(TestFiles.SAMPLE_XLSX))
+            {
+                PngViewOptions options = new PngViewOptions(pageFilePathFormat);
+                options.SpreadsheetOptions.RenderHeadings = true;
+
+                viewer.View(options, 1, 2, 3);
+            }
+
+            // TO PDF
+            pageFilePathFormat = Path.Combine(outputDirectory, "output.pdf");
+
+            using (Viewer viewer = new Viewer(TestFiles.SAMPLE_XLSX))
+            {
+                PdfViewOptions options = new PdfViewOptions(pageFilePathFormat);
+                options.SpreadsheetOptions.RenderHeadings = true;
+
+                viewer.View(options, 1, 2, 3);
             }
 
             Console.WriteLine($"\nSource document rendered successfully.\nCheck output in {outputDirectory}.");
