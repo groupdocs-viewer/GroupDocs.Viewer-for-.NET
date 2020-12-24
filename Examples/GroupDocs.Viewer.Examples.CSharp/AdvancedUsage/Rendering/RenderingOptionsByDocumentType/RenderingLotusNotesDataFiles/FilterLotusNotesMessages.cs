a87a17d2de4866fc178d91786c68b54418e4f9e3
Vladimir Litvinchik <vladimir.litvinchik@aspose.com>
2020-12-24 14:35:53 +0200

    Updated docs and examples to v20.12.

---
+++ Examples/GroupDocs.Viewer.Examples.CSharp/AdvancedUsage/Rendering/RenderingOptionsByDocumentType/RenderingLotusNotesDataFiles/FilterLotusNotesMessages.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using GroupDocs.Viewer.Options;
+
+namespace GroupDocs.Viewer.Examples.CSharp.AdvancedUsage.Rendering.RenderingOptionsByDocumentType.RenderingLotusNotesDataFiles
+{
+    /// <summary>
+    /// This example demonstrates how to filter messages by text/sender/recipient when rendering Lotus Notes data files.
+    /// </summary>
+    class FilterLotusNotesMessages
+    {
+        public static void Run()
+        {
+            string outputDirectory = Utils.GetOutputDirectoryPath();
+            string pageFilePathFormat = Path.Combine(outputDirectory, "page_{0}.html");
+
+            using (Viewer viewer = new Viewer(TestFiles.SAMPLE_NSF))
+            {
+                HtmlViewOptions options = HtmlViewOptions.ForEmbeddedResources(pageFilePathFormat);
+                //options.LotusNotesOptions.TextFilter = "April 2015";
+                options.LotusNotesOptions.AddressFilter = "campaign@banckle.com";
+
+                viewer.View(options);
+            }
+
+            Console.WriteLine($"\nSource document rendered successfully.\nCheck output in {outputDirectory}.");
+        }
+    }
+}
+
