a5cfa9c467d6f77ab829fad66827580e683e6c9c
Mikhail Evgrafov <mikhail.evgrafov@aspose.com>
2020-12-28 15:07:34 +0500

    "Example demonstrates how to download and render document" was updated.

--- Examples/GroupDocs.Viewer.Examples.CSharp/AdvancedUsage/Loading/LoadingDocumentsFromDifferentSources/LoadDocumentFromUrl.cs
+++ Examples/GroupDocs.Viewer.Examples.CSharp/AdvancedUsage/Loading/LoadingDocumentsFromDifferentSources/LoadDocumentFromUrl.cs
@@ -18,16 +18,18 @@ namespace GroupDocs.Viewer.Examples.CSharp.AdvancedUsage.Loading.LoadingDocument
 
             using (Viewer viewer = new Viewer(() => GetRemoteFile(url)))
             {
-                HtmlViewOptions options = HtmlViewOptions.ForEmbeddedResources(pageFilePathFormat);                
+                HtmlViewOptions options = HtmlViewOptions.ForEmbeddedResources(pageFilePathFormat);
                 viewer.View(options);
             }
 
             Console.WriteLine($"\nSource document rendered successfully.\nCheck output in {outputDirectory}.");
         }
-                
+
         private static Stream GetRemoteFile(string url)
         {
-            WebRequest request = WebRequest.Create(url);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0";
+            request.Timeout = 10000;
 
             using (WebResponse response = request.GetResponse())
                 return GetFileStream(response);
