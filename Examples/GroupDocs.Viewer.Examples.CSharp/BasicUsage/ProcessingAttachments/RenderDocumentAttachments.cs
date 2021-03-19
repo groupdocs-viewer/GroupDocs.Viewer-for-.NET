56c2a6f3bc065810fc5c3c7a9dc2a6f8fa54099e
Vladimir Litvinchik <vladimir.litvinchik@aspose.com>
2021-03-19 19:38:56 +0200

    Updated Viewer to v21.3.

--- Examples/GroupDocs.Viewer.Examples.CSharp/BasicUsage/ProcessingAttachments/RenderDocumentAttachments.cs
+++ Examples/GroupDocs.Viewer.Examples.CSharp/BasicUsage/ProcessingAttachments/RenderDocumentAttachments.cs
@@ -6,32 +6,46 @@ using GroupDocs.Viewer.Results;
 namespace GroupDocs.Viewer.Examples.CSharp.BasicUsage.ProcessingAttachments
 {
     /// <summary>
-    /// This example demonstrates how to render attachment into HTML.
+    /// This example demonstrates how to render email message attachments into HTML.
     /// </summary>
     class RenderDocumentAttachments
     {
         public static void Run()
         {
             string outputDirectory = Utils.GetOutputDirectoryPath();
-            string pageFilePathFormat = Path.Combine(outputDirectory, "page_{0}.html");
-
-            Attachment attachment = new Attachment("attachment-word.doc");
-            MemoryStream attachmentStream = new MemoryStream();
 
             using (Viewer viewer = new Viewer(TestFiles.SAMPLE_MSG_WITH_ATTACHMENTS))
             {
-                viewer.SaveAttachment(attachment, attachmentStream);
+                var attachments = viewer.GetAttachments();
+
+                foreach (var attachment in attachments)
+                {
+                    MemoryStream attachmentStream = new MemoryStream();
+                    viewer.SaveAttachment(attachment, attachmentStream);
+
+                    RenderAttachment(attachment, attachmentStream, outputDirectory);
+                }
             }
 
-            using (Viewer viewer = new Viewer(attachmentStream))
+            Console.WriteLine($"\nAttachment rendered successfully.\nCheck output in {outputDirectory}.");
+        }
+
+        private static void RenderAttachment(Attachment attachment, MemoryStream attachmentStream, 
+            string outputDirectory)
+        {
+            string pageFilePathFormat = Path.Combine(outputDirectory, "page_{0}.html");
+
+            string extension = Path.GetExtension(attachment.FileName);
+            FileType fileType = FileType.FromExtension(extension);
+            LoadOptions loadOptions = new LoadOptions(fileType);
+            
+            using (Viewer viewer = new Viewer(attachmentStream, loadOptions))
             {
                 HtmlViewOptions options =
                     HtmlViewOptions.ForEmbeddedResources(pageFilePathFormat);
 
                 viewer.View(options);
             }
-
-            Console.WriteLine($"\nAttachment rendered successfully.\nCheck output in {outputDirectory}.");
         }
     }
 }
